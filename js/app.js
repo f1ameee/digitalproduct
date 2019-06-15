7f34097b6d168c8fb593c16c525e596a2c8d60a8
f1ameee <chern1kov1113@gmail.com>
2019-06-15 15:17:38 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+;
+(function () {
+
+	let heroSlider = $('.single-slide');
+
+	heroSlider.slick({
+		dots: true,
+		arrows: true,
+		fade: true,
+	});
+
+	let imageSlider = $('.single-slide');
+
+	imageSlider.slick({
+		dots: true,
+		arrows: true,
+	});
+
+
+
+
+
+})();
